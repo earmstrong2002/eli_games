081684ce2716e27995a1d169ad0e3196554612e0
Eli Armstrong <138740082+earmstrong2002@users.noreply.github.com>
2023-07-15 21:32:26 -0700

    began implementation of image graphics
    -Assigned image objects to move objects
    -Updated requirements.txt to include Pillow
    -tweaked window layound
    -added Rps.draws for tie games

--- rpsls/rpsls.py
+++ rpsls/rpsls.py
@@ -1,12 +1,16 @@
 import random as rand
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image
+from pathlib import Path
+from sys import platform
 
 class Move:
-    def __init__(self, title, beats=None, actions=None):
+    def __init__(self, title, beats=None, actions=None, texture=None):
         self.beats = beats
         self.title = title
         self.actions = actions
+        self.texture = texture
                 
     def __str__(self):
         return self.title
@@ -26,6 +30,9 @@ def initialize_moves():
     LIZARD = Move('lizard')
     SPOCK = Move('spock')
     
+    # list for iterating through all moves
+    MOVES = [ROCK, PAPER, SCISSORS, LIZARD, SPOCK]
+    
     # defining which moves each move beats
     ROCK.beats = (SCISSORS, LIZARD)
     PAPER.beats = (ROCK, SPOCK)
@@ -40,13 +47,30 @@ def initialize_moves():
     LIZARD.actions = ('eats', 'poisons')
     SPOCK.actions = ('vaporizes', 'smashes')
     
-    MOVES = [ROCK, PAPER, SCISSORS, LIZARD, SPOCK]
+    # getting file directories for associated move textures
+    pths_textures = list(Path('rpsls/textures/').glob('*'))
+    strs_textures = []
+    for pth in pths_textures: # convert path objects to strings for processing
+        strs_textures.append(str(pth))
+    if platform.startswith('win'): # replace \ with / for compatability
+        for i in range(len(strs_textures)):
+            strs_textures[i] = strs_textures[i].replace('\\', '/')
+            
+    filenames_textures = []
+    for pth in strs_textures: # isolate file name, without extension
+        im_name = pth.split('/')[-1][:-4]
+        filenames_textures.append(im_name)
+    
+    for move in MOVES: # assign textures to appropriate move objects
+        index = filenames_textures.index(move.title)
+        move.texture = Image.open(pths_textures[index])
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.geometry('600x360')
         self.title('Play R.P.S.L.S.')
+        self.resizable(0, 0) # not resizeable, for simplicity
         #TODO FINALIZE LAYOUT
         #TODO incorporate ttk styles
         #TODO add GRAPHICS
@@ -77,29 +101,53 @@ class App(tk.Tk):
         
     def make_display(self):
         # configure frame for display
-        self.frm_display = ttk.Frame(self.frm_main, relief='groove', border=10)
+        self.frm_display = ttk.Frame(self.frm_main)
         self.frm_display.columnconfigure((0, 1, 2), weight=1, pad=5)
-        self.frm_display.rowconfigure((0, 1), weight=1, pad=5)
+        self.frm_display.rowconfigure(0, weight=0, minsize=50, pad=5)
+        self.frm_display.rowconfigure(1, weight=1, pad=5)
         self.frm_display.grid(column=0, row=0, sticky='nsew',)
         
-        # populate display        
+        # scoreboard       
+        self.scoreboard = tk.StringVar(
+            value=f'{rps.player_wins} -- {rps.draws} -- {rps.com_wins}'
+            )
         self.lbl_scoreboard = ttk.Label(
             self.frm_display,
-            text=f'Player wins: 0, Com wins: 0'
-        )
+            textvariable=self.scoreboard
+            )
         self.lbl_scoreboard.grid(column = 1, row=0)
         
+        # player label
+        self.lbl_player = ttk.Label(self.frm_display, text='Player')
+        self.lbl_player.grid(column=0, row=0, sticky='e')
+        
+        # computer label
+        self.lbl_com = ttk.Label(self.frm_display, text='Computer')
+        self.lbl_com.grid(column=2, row=0, sticky='w')
+        
+        self.make_screen()
+        
+    def make_screen(self):
+        pass
+    
     def increment_scoreboard(self):
-        self.lbl_scoreboard['text'] = f'Player wins: {rps.player_wins}, Com wins: {rps.com_wins}'
+        self.scoreboard.set(
+            f'{rps.player_wins} -- {rps.draws} -- {rps.com_wins}'
+            )
         
     def make_move_picker(self):
         # configure frame for move picker
-        self.frm_move_picker = ttk.Frame(self.frm_main)
+        self.frm_move_picker = ttk.LabelFrame(
+            self.frm_main,
+            text='Pick Your Move',
+            labelanchor='n'
+            )
         self.frm_move_picker.rowconfigure(0, weight=1, pad=5)
         self.frm_move_picker.columnconfigure((0, 1, 2, 3, 4), weight=1, pad=5)
         self.frm_move_picker.grid(column=0, row=1, sticky='nsew')
         
         # populate move picker
+        #TODO make move button constructor modular
         btn_rock = ttk.Button(self.frm_move_picker, text='ROCK',
                              command=lambda: run_game(ROCK))
         btn_rock.grid(column=0, row=0, sticky='nsew')
@@ -127,6 +175,7 @@ class Rps():
 
         }
         self.player_wins = 0
+        self.draws = 0
         self.com_wins = 0
 
     def com_decide(self):
@@ -160,6 +209,7 @@ class Rps():
         match victor[0][0]:
             case 'p': self.player_wins += 1
             case 'c': self.com_wins += 1
+            case 'd': self.draws += 1
 
 def run_game(player_move):
     print(player_move)
